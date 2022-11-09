cc7421e7063273ffb8666a3599d2a8bcc7291f41
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-11-09 14:33:26 +0600

    review get api created

--- index.js
+++ index.js
@@ -36,6 +36,19 @@ async function run() {
     });
 
     // reviews api
+
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
     app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewsCollection.insertOne(review);
